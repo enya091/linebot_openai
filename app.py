8e33476267ff5df2a7a347b52285148be2d19ca1
enya091 <113331835+enya091@users.noreply.github.com>
2024-12-08 18:33:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import *
+from linebot.models import TemplateSendMessage, ButtonsTemplate, PostbackAction, TextSendMessage
 import os
 import openai
 import traceback
